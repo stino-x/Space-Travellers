93c18476d5ecd48f8a478e9cba750be3c4fa0703
stino-x <107426247+stino-x@users.noreply.github.com>
2023-08-31 22:53:34 +0200

    profile structure

--- src/Components/Profile/myProfile.js
+++ src/Components/Profile/myProfile.js
@@ -1,7 +1,46 @@
 import React from 'react';
+import './myProfile.css';
+import Table from 'react-bootstrap/Table';
 
-const MyProfile = () => (
-  <div>myProfile under construction</div>
-);
-
-export default MyProfile;
+export default function myProfile() {
+  return (
+    <div className="container">
+      <Table className="table-1">
+        <thead>
+          <tr className="header-cell">
+            <th colSpan={4}>#</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr className="border">
+            <td>1</td>
+          </tr>
+          <tr className="border">
+            <td>2</td>
+          </tr>
+          <tr className="border">
+            <td>3</td>
+          </tr>
+        </tbody>
+      </Table>
+      <Table className="table-2">
+        <thead>
+          <tr className="header-cell">
+            <th colSpan={4}>#</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr className="border">
+            <td>1</td>
+          </tr>
+          <tr className="border">
+            <td>2</td>
+          </tr>
+          <tr className="border">
+            <td>3</td>
+          </tr>
+        </tbody>
+      </Table>
+    </div>
+  );
+}
